b1418edbaa67b069958224dc187a06a982705316
jit-webapp <webapp.jit@gmail.com>
2026-03-16 19:37:13 +0700

    8.6.56.1

--- version.js
+++ version.js
@@ -3,7 +3,7 @@
 // ============================================
 
 // เวอร์ชันหลัก
-var APP_VERSION = 'v8.6.56';
+var APP_VERSION = 'v8.6.56.1';
 var APP_LAST_UPDATED = '14-03-2026';
 var APP_LAST_UPDATED_TH = '14 มีนาคม 2569';
 
